24c10445b0508b96e4445cf05a771401eca8bb81
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-01 09:28:01 -0700

    added file uc1

---
+++ EmployeeWageProblems/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace UseCase1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Emp e = new Emp();
+            e.Atttendece();
+        }
+    }
+    class Emp
+    {
+        public void Atttendece()
+        {
+            Random r = new Random();
+            int value = r.Next(0, 2);//only generate values bt 0-2 means 0,1 ;
+            // 0 is for present;
+            // 1 is for absent;
+            if (value == 0)
+            {
+                Console.WriteLine("Present");
+            }
+            else
+            {
+                Console.WriteLine("Absent");
+            }
+        }
+    }
+}
